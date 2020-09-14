c75bc5afae2dc81d29d5f96abfadb20d69e99cdd
DLSUSanti <santiago_alvarez@dlsu.edu.ph>
2020-09-14 23:03:17 +0800

    dashboard page added

--- mysite/testmqtt/urls.py
+++ mysite/testmqtt/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('dashboard', views.dashboard, name='dashboard'),
     path('getCurrentValue', views.getCurrentValue, name='getCurrentValue'),
 ]
--- mysite/testmqtt/views.py
+++ mysite/testmqtt/views.py
@@ -4,55 +4,66 @@ import paho.mqtt.client as mqtt
 import json
 import datetime
 
-sensorA = '0,0,0,0,0,0'
-sensorB = '0,0,0,0,0,0'
-sensorC = '0,0,0,0,0,0'
-sensorD = '0,0,0,0,0,0'
+# sensorA = '0,0,0,0,0,0'
+# sensorB = '0,0,0,0,0,0'
+# sensorC = '0,0,0,0,0,0'
+# sensorD = '0,0,0,0,0,0'
+messageArray = []
 
 
 def getCurrentValue(request):
-    global sensorA
-    global sensorB
-    global sensorC
-    global sensorD
+    # global sensorA
+    # global sensorB
+    # global sensorC
+    # global sensorD
+    global messageArray
+    #
+    #JSONer = {}
+    # JSONer['sensorA'] = sensorA
+    # JSONer['sensorB'] = sensorB
+    # JSONer['sensorC'] = sensorC
+    # JSONer['sensorD'] = sensorD
 
-    JSONer = {}
-    JSONer['sensorA'] = sensorA
-    JSONer['sensorB'] = sensorB
-    JSONer['sensorC'] = sensorC
-    JSONer['sensorD'] = sensorD
-
-    return HttpResponse(json.dumps(JSONer))
+    #return HttpResponse(json.dumps(JSONer))
+    JSONsend = json.dumps(messageArray)
+    messageArray = []
+    return HttpResponse(JSONsend)
 
 
 def index(request):
     return render(request, 'testmqtt/index.html')
 
 
+def dashboard(request):
+    return render(request, 'testmqtt/dashboard.html')
+
+
 def on_message(client, userdata, msg):
-    global sensorA
-    global sensorB
-    global sensorC
-    global sensorD
-
+    # global sensorA
+    # global sensorB
+    # global sensorC
+    # global sensorD
+    global messageArray
+    #
     arrived = json.loads(msg.payload)
+    #
+    # datastring = ""
+    # for number in arrived['IMU']:
+    #     datastring += str(number) + ","
+    # for number in arrived['Gyro']:
+    #     datastring += str(number) + ","
+    #
+    # sensor = arrived['device']
+    # if sensor == "A":
+    #     sensorA = datastring
+    # if sensor == "B":
+    #     sensorB = datastring
+    # if sensor == "C":
+    #     sensorC = datastring
+    # if sensor == "D":
+    #     sensorD = datastring
 
-    datastring = ""
-    for number in arrived['IMU']:
-        datastring += str(number) + ","
-    for number in arrived['Gyro']:
-        datastring += str(number) + ","
-
-    sensor = arrived['device']
-    if sensor == "A":
-        sensorA = datastring
-    if sensor == "B":
-        sensorB = datastring
-    if sensor == "C":
-        sensorC = datastring
-    if sensor == "D":
-        sensorD = datastring
-
+    messageArray.append(arrived)
     pass
 
 
