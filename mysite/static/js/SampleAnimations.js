272afa11a05c83adf1872eee87900c320716ab57
DLSUSanti <santiago_alvarez@dlsu.edu.ph>
2020-12-01 13:05:25 +0800

    modified sample animation

--- mysite/static/js/SampleAnimations.js
+++ mysite/static/js/SampleAnimations.js
@@ -90,6 +90,70 @@ samplecsv.push(new CSVRow(4, 'NE3', 180, 400, -100))
 samplecsv.push(new CSVRow(4, 'SW3', -20, 400, 100))
 samplecsv.push(new CSVRow(4, 'SE3', 180, 400, 100))
 
+//time = 5
+samplecsv.push(new CSVRow(5, 'NW1', -100, 0, -100))
+samplecsv.push(new CSVRow(5, 'NE1', 100, 0, -100))
+samplecsv.push(new CSVRow(5, 'SW1', -100, 0, 100))
+samplecsv.push(new CSVRow(5, 'SE1', 100, 0, 100))
+
+samplecsv.push(new CSVRow(5, 'NW2', -70, 200, -100))
+samplecsv.push(new CSVRow(5, 'NE2', 130, 200, -100))
+samplecsv.push(new CSVRow(5, 'SW2', -70, 200, 100))
+samplecsv.push(new CSVRow(5, 'SE2', 130, 200, 100))
+
+samplecsv.push(new CSVRow(5, 'NW3', -40, 400, -100))
+samplecsv.push(new CSVRow(5, 'NE3', 160, 400, -100))
+samplecsv.push(new CSVRow(5, 'SW3', -40, 400, 100))
+samplecsv.push(new CSVRow(5, 'SE3', 160, 400, 100))
+
+//time = 6
+samplecsv.push(new CSVRow(6, 'NW1', -100, 0, -100))
+samplecsv.push(new CSVRow(6, 'NE1', 100, 0, -100))
+samplecsv.push(new CSVRow(6, 'SW1', -100, 0, 100))
+samplecsv.push(new CSVRow(6, 'SE1', 100, 0, 100))
+
+samplecsv.push(new CSVRow(6, 'NW2', -80, 200, -100))
+samplecsv.push(new CSVRow(6, 'NE2', 120, 200, -100))
+samplecsv.push(new CSVRow(6, 'SW2', -80, 200, 100))
+samplecsv.push(new CSVRow(6, 'SE2', 120, 200, 100))
+
+samplecsv.push(new CSVRow(6, 'NW3', -60, 400, -100))
+samplecsv.push(new CSVRow(6, 'NE3', 140, 400, -100))
+samplecsv.push(new CSVRow(6, 'SW3', -60, 400, 100))
+samplecsv.push(new CSVRow(6, 'SE3', 140, 400, 100))
+
+//time = 7
+samplecsv.push(new CSVRow(7, 'NW1', -100, 0, -100))
+samplecsv.push(new CSVRow(7, 'NE1', 100, 0, -100))
+samplecsv.push(new CSVRow(7, 'SW1', -100, 0, 100))
+samplecsv.push(new CSVRow(7, 'SE1', 100, 0, 100))
+
+samplecsv.push(new CSVRow(7, 'NW2', -90, 200, -100))
+samplecsv.push(new CSVRow(7, 'NE2', 110, 200, -100))
+samplecsv.push(new CSVRow(7, 'SW2', -90, 200, 100))
+samplecsv.push(new CSVRow(7, 'SE2', 110, 200, 100))
+
+samplecsv.push(new CSVRow(7, 'NW3', -80, 400, -100))
+samplecsv.push(new CSVRow(7, 'NE3', 120, 400, -100))
+samplecsv.push(new CSVRow(7, 'SW3', -80, 400, 100))
+samplecsv.push(new CSVRow(7, 'SE3', 120, 400, 100))
+
+//time = 8
+samplecsv.push(new CSVRow(8, 'NW1', -100, 0, -100))
+samplecsv.push(new CSVRow(8, 'NE1', 100, 0, -100))
+samplecsv.push(new CSVRow(8, 'SW1', -100, 0, 100))
+samplecsv.push(new CSVRow(8, 'SE1', 100, 0, 100))
+
+samplecsv.push(new CSVRow(8, 'NW2', -100, 200, -100))
+samplecsv.push(new CSVRow(8, 'NE2', 100, 200, -100))
+samplecsv.push(new CSVRow(8, 'SW2', -100, 200, 100))
+samplecsv.push(new CSVRow(8, 'SE2', 100, 200, 100))
+
+samplecsv.push(new CSVRow(8, 'NW3', -100, 400, -100))
+samplecsv.push(new CSVRow(8, 'NE3', 100, 400, -100))
+samplecsv.push(new CSVRow(8, 'SW3', -100, 400, 100))
+samplecsv.push(new CSVRow(8, 'SE3', 100, 400, 100))
+
 var timeCoefficient = 60; //time between frames
 
 for(var i = 0; i < samplecsv.length; i++){
